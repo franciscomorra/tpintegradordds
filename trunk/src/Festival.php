10d55becfed97434a5d463104e23d59084004099
ffernandocarril@gmail.com <ffernandocarril@gmail.com@aa0e8deb-e06a-5a9c-535b-127f3725d98a>
2013-11-02 04:58:57 +0000

    Introduccion de ORM

---
+++ trunk/src/Festival.php
@@ -0,0 +1,46 @@
+<?php
+
+/**
+ * @Entity @Table(name="festivales")
+ **/
+class Product
+{
+    /** @Id @Column(type="integer") @GeneratedValue **/
+    protected $id;
+    /** @Column(type="string") **/
+    protected $nombre;
+
+    /**
+     * @param string $nombre
+     */
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+    }
+
+    /**
+     * @return string
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+}
+
